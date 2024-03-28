c07da2a8745212853bf09ed8375896001c8963e8
unknown <denzelkinyua11@gmail.com>
2024-03-28 21:18:56 +0300

    Third Commit: Creation of models

--- marks-backend/main.py
+++ marks-backend/main.py
@@ -1,11 +1,43 @@
 from flask import request, jsonify, Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token
 from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 api = Api(app, doc="/docs")
+
+# Models {Serialized}
+student_model = api.models(
+    "Students",
+    {
+        "id": fields.Integer(),
+        "username": fields.String(),
+        "email": fields.String(),
+        "password": fields.String()
+    }
+)
+
+teacher_model = api.models(
+    "Teachers",
+    {
+        "id": fields.Integer(),
+        "username": fields.String(),
+        "email": fields.String(),
+        "password": fields.String()
+    }
+)
+
+marks_model = api.models(
+    "Marks",
+    {
+        "id": fields.Integer(),
+        "subject": fields.String(),
+        "score": fields.Integer(),
+        "teacher_id":fields.Integer(),
+        "student_id": fields.Integer()
+    }
+)
 
 @api.route("/hello")
 class HelloResource(Resource):
---
+++ marks-backend/model.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    marks = db.relationship('Mark', backref='teacher', lazy=True)
+
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    marks = db.relationship('Mark', backref='student', lazy=True)
+
+class Mark(db.Model):
+    __tablename__ = 'marks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(50), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
