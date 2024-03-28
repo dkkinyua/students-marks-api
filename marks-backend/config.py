9ddad181b436c3387eedbc04ab12f742318521cb
unknown <denzelkinyua11@gmail.com>
2024-03-28 14:54:54 +0300

    Third commit: Configuration

--- marks-backend/config.py
+++ marks-backend/config.py
@@ -1,4 +1,22 @@
 from dotenv import load_dotenv
-import os 
+import os
 
+
+load_dotenv()
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+class Config():
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(BASE_PATH, "dev.db")
+    SQLALCHEMY_ECHO = False
+    DEBUG = True
+
+class TestConfig(Config):
+    pass
+
+class ProductionConfig(Config):
+    pass
+    
