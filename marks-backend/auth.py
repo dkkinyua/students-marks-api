be61ccf871cbe77535ee158190c35a50ebed0c90
unknown <denzelkinyua11@gmail.com>
2024-04-06 23:00:34 +0300

    Commit: Update to the auth namespace

--- marks-backend/auth.py
+++ marks-backend/auth.py
@@ -1,6 +1,7 @@
 # A namespace for user authentication
 from flask import request, jsonify, make_response
 from flask_restx import Resource, Namespace, fields
+from flask_jwt_extended import create_access_token, create_refresh_token
 from werkzeug.security import check_password_hash, generate_password_hash
 from model import Student, Teacher
 
@@ -34,7 +35,8 @@ login_model = auth_ns.model(
     }
 )
 
-@auth_ns.route("/signup", methods=["POST"])
+#Student Signup route
+@auth_ns.route("/signup/student", methods=["POST"])
 class StudentSignupResource(Resource):
     @auth_ns.expect(student_model)
     def post(self):
@@ -46,6 +48,7 @@ class StudentSignupResource(Resource):
             return jsonify({
                 "message": f"{name} exists!"
             })
+        
         new_student = Student(
             name=data.get("name"),
             email=data.get("email"),
@@ -56,3 +59,63 @@ class StudentSignupResource(Resource):
         return make_response(jsonify({
             "message": f"Student {name} has been created"
         }), 201)
+    
+# Teacher Signup Route
+@auth_ns.route("/signup/teacher", methods=["POST"])
+class TeacherSignupResource(Resource):
+    @auth_ns.expect(teacher_model)
+    def post(self):
+        data = request.get_json()
+        name = data.get("name")
+        db_teacher = Teacher.query.filter_by(name=name).first()
+
+        if db_teacher is not None:
+            return jsonify({
+                "message": f"{name} exists!"
+            })
+        
+        new_teacher = Teacher(
+            name=data.get("name"),
+            email=data.get("email"),
+            password=generate_password_hash(data.get("password"))
+        )
+
+        new_teacher.save()
+        return make_response(jsonify({
+            "message": f"Teacher {name} has been created"
+        }), 201)
+    
+#Student Login Route
+@auth_ns.route("/login/student", methods=["POST"])
+class StudentLoginRoute(Resource):
+    @auth_ns.expect(login_model)
+    def post(self):
+        data = request.get_json()
+        name = data.get("name")
+        password = data.get("password")
+
+        db_student = Student.query.filter_by(name=name).first()
+        if db_student and check_password_hash(db_student.password, password):
+            return jsonify({
+                "message": "User logged in."
+            })
+        
+# Teacher Login Route
+@auth_ns.route("/login/teacher", methods=["POST"])
+class TeacherLoginRoute(Resource):
+    @auth_ns.expect(login_model)
+    def post(self):
+        data = request.get_json()
+        name = data.get("name")
+        password = data.get("password")
+
+        db_teacher = Teacher.query.filter_by(name=name).first()
+        if db_teacher and check_password_hash(db_teacher.password, password):
+            access_token = create_access_token(identity=db_teacher.name)
+            refresh_token = create_refresh_token(identity=db_teacher.name)
+
+            return jsonify({
+                "message": "Teacher logged in",
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            })
