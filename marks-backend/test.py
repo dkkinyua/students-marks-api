2c07ac810bb37f665e70ac47ad23a4d5dd3008e5
unknown <denzelkinyua11@gmail.com>
2024-04-07 15:37:59 +0300

    Commit: Update to test.py to test student signup route

--- marks-backend/test.py
+++ marks-backend/test.py
@@ -21,9 +21,26 @@ class APITestCase(unittest.TestCase):
 
         self.assertEqual(json, {"message": "Hello World"})
 
+    # Test: Student Signup route
+    def test_student_signup(self):
+        test_user = {
+            "name": "testuser",
+            "email": "testuser@skuli.com",
+            "password": "testuser1"
+        }
+
+        signup_response = self.client.post("/auth/signup/student", json=test_user)
+
+        status_code = signup_response.status_code
+
+        self.assertEqual(status_code, 201)
+        
+
     # Tear down db after tests
     def tearDown(self):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
-        
+
+if __name__ == "__main__":
+    unittest.main()
